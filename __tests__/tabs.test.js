d8b8b8aa4e8885eb8a264b2c8a7f986ba5f00ff2
freek <freek@spatie.be>
2017-05-04 19:57:37 +0200

    commit

--- __tests__/tabs.test.js
+++ __tests__/tabs.test.js
@@ -1,14 +1,18 @@
-import Vue from 'vue';
+import { beforeEachHooks, afterEachHooks, mount } from 'vue-unit';
+
+import Tabs from '../src/components/Tabs.vue';
 
 describe('save-state', () => {
 
     beforeEach(() => {
-
+        beforeEachHooks();
     });
 
-    it('can test', () => {
-       expect(true).toBeTruthy();
+    afterEach(() => {
+        afterEachHooks();
     });
 
-
+    it('can mount tabs', () => {
+        mount(Tabs);
+    });
 });
--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -27,4 +27,5 @@
             this.isActive = this.selected;
         },
     }
+
 </script>
